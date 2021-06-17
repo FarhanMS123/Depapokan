35bf0707caa718938aae3b36c4259d87f1cada9e
Farhan Sabran <FarhanMS123@users.noreply.github.com>
2021-06-17 16:17:27 +0700

    Develope API

--- app/Http/Controllers/DepapokanController.php
+++ app/Http/Controllers/DepapokanController.php
@@ -3,20 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 use App\Models\Padepokan;
 use App\Models\Ulasan;
 
 class DepapokanController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $padepokans = Padepokan::all();
 
+        if($request->wantsJson()){
+            foreach ($padepokans as $padepokan) {
+                $padepokan->gambar = asset($padepokan->gambar);
+                $padepokan->ulasan = $padepokan->ulasan;
+            }
+            return response()->json($padepokans, 200);
+            // return $padepokans->toJson(JSON_PRETTY_PRINT);
+        }
+
+        return view('index', compact("padepokans"));
+    }
+
+    public function dapatSatu($id, Request $request){
+        $padepokan = Padepokan::findOrFail($id);
+
+        if($request->wantsJson()){
+            $padepokan->gambar = asset($padepokan->gambar);
+            $padepokan->ulasan = $padepokan->ulasan;
+            return response()->json($padepokan, 200);
+            // return $padepokans->toJson(JSON_PRETTY_PRINT);
+        }
+
+        $padepokans = [$padepokan];
         return view('index', compact("padepokans"));
     }
 
     public function hapus($id, Request $request){
         Padepokan::destroy($id);
+
+        if($request->wantsJson())
+            return response()->json(["id"=>$id, "saka"=>"hapus", "palang"=>"berhasil"], 200);
 
         return back();
     }
@@ -34,11 +61,17 @@ class DepapokanController extends Controller
 
         $gambar = $request->file("gambar")->store("padepokan");
 
-        Padepokan::create([
+        $padepokan = Padepokan::create([
             "gambar"    => $gambar,
             "nama"      => $request->nama,
             "deskripsi" => $request->deskripsi
         ]);
+
+        $padepokan->gambar = asset($padepokan->gambar);
+        $padepokan->ulasan = [];
+        if($request->wantsJson())
+            return response()->json($padepokan, 200);
+            // return response()->json(["saka"=>"tambah", "palang"=>"berhasil"], 200);
 
         return redirect("/");
     }
@@ -63,13 +96,25 @@ class DepapokanController extends Controller
         $padepokan->deskripsi = $request->deskripsi;
         $padepokan->save();
 
+        if($request->wantsJson())
+            return response()->json($padepokan, 200);
+
         return redirect("/");
     }
+
+    // ##### ULASAN #########################################
 
     public function viewUlasan($id, Request $request){
         $padepokan = Padepokan::findOrFail($id);
 
         return view('ulasan', compact('padepokan', 'id'));
+    }
+
+    public function langkahSatu($id, $uid, Request $request){
+        $ulasan = Ulasan::findOrFail($uid);
+        $ulasan->padepokan = $ulasan->padepokan;
+
+        return response()->json($ulasan, 200);
     }
 
     public function ulas($id, Request $request){
@@ -80,10 +125,13 @@ class DepapokanController extends Controller
 
         $padepokan = Padepokan::findOrFail($id);
 
-        $padepokan->ulasan()->create([
+        $ulasan = $padepokan->ulasan()->create([
             'name' => $request->nama,
             'comment' => $request->ulasan
         ]);
+
+        if($request->wantsJson())
+            return response()->json($ulasan, 200);
 
         return back();
     }
@@ -94,16 +142,24 @@ class DepapokanController extends Controller
             'ulasan' => ['required', 'string']
         ]);
 
-        Ulasan::findOrFail($uid)->update([
+        $ulasan = Ulasan::findOrFail($uid)->update([
             'name' => $request->nama,
             'comment' => $request->ulasan
         ]);
+
+        if($request->wantsJson()){
+            $ulasan->padepokan = $ulasan->padepokan;
+            return response()->json($ulasan, 200);
+        }
 
         return back();
     }
 
     public function hapusUlasan($id, $uid, Request $request){
         Ulasan::destroy($uid);
+
+        if($request->wantsJson())
+            return response()->json(["id"=>$id, "saka"=>"hapus", "palang"=>"berhasil"], 200);
 
         return back();
     }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\DepapokanController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get("/items", [DepapokanController::class, 'index']);
+
+Route::post("/item", [DepapokanController::class, 'tambah']);
+Route::get("/item/{id}", [DepapokanController::class, 'dapatSatu']);
+Route::patch("/item/{id}", [DepapokanController::class, 'ubah']);
+Route::delete("/item/{id}", [DepapokanController::class, 'hapus']);
+
+Route::post("/item/{id}/ulasan", [DepapokanController::class, 'ulas']);
+Route::get("/item/{id}/ulasan/{uid}", [DepapokanController::class, 'langkahSatu']);
+Route::patch("/item/{id}/ulasan/{uid}", [DepapokanController::class, 'ubahUlasan']);
+Route::delete("/item/{id}/ulasan/{uid}", [DepapokanController::class, 'hapusUlasan']);
